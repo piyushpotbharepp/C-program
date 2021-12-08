3ee31e71d302fa0afe820284d851e4906cf261d0
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2021-12-08 19:20:43 +0530

    Row & Column Pattern 7

---
+++ ZABCD.C
@@ -0,0 +1,22 @@
+/* A
+   A B
+   A B C
+   A B C D */
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+int r,c;
+clrscr();
+for(r='A';r<='D';r++)
+{
+	for(c='A';c<=r;c++)
+	{
+		printf("%c",c);
+	}
+	printf("\n");
+}
+getch();
+}
