717dfa3068aae4d94bb70240ed14e1caea3727a5
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2021-12-10 11:46:43 +0530

    Print 5 Elements Array

---
+++ 5ARRAY.C
@@ -0,0 +1,23 @@
+// Write a program to print 5 elements array.
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+clrscr();
+int a[4];
+a[0]=2;
+a[1]=3;
+a[2]=4;
+a[3]=8;
+a[4]=10;
+
+printf("%d\n",a[0]);
+printf("%d\n",a[1]);
+printf("%d\n",a[2]);
+printf("%d\n",a[3]);
+printf("%d\n",a[4]);
+
+getch();
+}
