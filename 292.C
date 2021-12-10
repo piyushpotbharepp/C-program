60591e52b79682b002ffde706d64fc855911707b
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2021-12-10 16:35:10 +0530

    Row & Column Pattern 22

---
+++ 292.C
@@ -0,0 +1,23 @@
+/* 1 2 3 4 5
+   1 2 3 4
+   1 2 3
+   1 2
+   1        */
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+int r,c;
+clrscr();
+for(r=5;r>=1;r--)
+{
+	for(c=1;c<=r;c++)
+	{
+		printf("%d",c);
+	}
+	printf(" \n");
+}
+getch();
+}
