eed3b0e4c971b171abab75549d9adb77808ff11a
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2021-12-17 16:00:49 +0530

    Read Nickname & Store in Array

---
+++ NIC.C
@@ -0,0 +1,16 @@
+// Program to read nickname and store in array and print it.
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+char nic[20];
+clrscr();
+
+printf("Enter Your Nickname : ");
+scanf("%s",&nic);
+printf("%s",nic);
+
+getch();
+}
