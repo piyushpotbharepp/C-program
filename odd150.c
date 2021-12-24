9bc913dda6b4bfdd951cc08c225557b3370e6c03
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2021-12-24 16:22:03 +0530

    Print Odd Numbers Between 1 to 50

---
+++ odd150.c
@@ -0,0 +1,18 @@
+// Write a C program that prints all even numbers between 1 and 50 (inclusive).
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int i;
+	printf("PRINT ALL THE ODD NUMBERS BETWEEN 1 TO 50 :");
+	
+	for(i=0;i<=50;i++)
+	{
+		if(i%2!=0)
+		{
+			printf("\n%d",i);
+		}
+	}
+}
