7b1834827522941bdde94a4041104ea498d8b39d
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2021-12-24 16:41:14 +0530

    Program To Display Name 10 Times On The Screen

---
+++ 10name.c
@@ -0,0 +1,13 @@
+// Write a program to display your name 10 times on the screen
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int i;
+	for(i=0;i<=4;i++)
+	{
+		printf("\nRamesh Shahu");
+	}
+}
