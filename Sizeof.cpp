f6c1d31283a341be206a1316ea2512bcc10a63d7
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-03-14 00:10:53 +0530

    Understanding sizeof

---
+++ Sizeof.cpp
@@ -0,0 +1,14 @@
+// Understanding sizeof 
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	printf("Int : %ld \n", sizeof(int));
+	printf("Float : %ld \n", sizeof(float));
+	printf("Double : %ld \n", sizeof(double));
+	printf("Char : %ld \n", sizeof(char));
+	
+	return 0;
+}
