bbbdf5f2f920aaa84ecf8a7170b591342971d1b8
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-03-18 12:34:00 +0530

    Understanding Static Variable

---
+++ Static Variable.cpp
@@ -0,0 +1,26 @@
+/*
+ 	Understanding the static variable
+	------------------------------------
+	A static variable is able to retain its value between 
+	different function calls.
+	
+*/
+
+#include <stdio.h>
+
+void say_hello();
+
+int main(){
+	int i;
+	for(i=0;i<5;i++)
+	{
+		say_hello();
+	}
+	return 0;
+}
+void say_hello(){
+	static int num_calls = 1;
+	
+	printf("Hello number %d\n", num_calls);
+	num_calls++;
+}
