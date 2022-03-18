e329f2a6c5e8c6d759005da99fcdbe1e3e802594
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-03-18 19:04:12 +0530

    Understanding Accessing Array Elements

---
+++ Accessing Array Elements.cpp
@@ -0,0 +1,15 @@
+/* 
+	Understanding Accessing the array elemnets
+	-------------------------------------------------
+	- 	The contents of an arary are called elements with each 
+		elements accessible by an index number
+	- 	Index number starts at 0.
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	int x[5] = {20, 45, 16, 18 ,22};
+	printf("The second element is %d\n", x[1]);
+}
