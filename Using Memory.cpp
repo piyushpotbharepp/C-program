a6c7787305ebe01b5e8e5c56dee80e246f8ec392
piyushpotbharepp <95415292+piyushpotbharepp@users.noreply.github.com>
2022-03-18 19:41:25 +0530

    Understanding Using Memory

---
+++ Using Memory.cpp
@@ -0,0 +1,17 @@
+/* 
+	Understandind Using Memory
+	---------------------------------------
+	C is designed to be a low level language that can be
+	easily access memory locations and perform memory related operations.
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{    
+    int num;
+	printf("Enter a number : ");
+	scanf("%d", &num);
+	printf("%d",num);     
+}
